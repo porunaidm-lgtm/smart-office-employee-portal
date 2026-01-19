9e4f8870d344d37c049dddcd208dbdb75000eedc
porunaidm-lgtm <porunai.dm@gmail.com>
2026-01-20 02:58:35 +0530

    Update axiosClient.js

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://smart-office-employee-portal.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
